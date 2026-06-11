82edc302659a3d48329efec58883e246563b3874
Rahul Rajput <rahulrajput20112005@gmail.com>
2026-06-11 17:31:26 +0530

    worked on config file

--- config/database.php
+++ config/database.php
@@ -1,16 +1,4 @@
-<?php
 
-// $host = "localhost";
-// $user = "root";
-// $pass = "";
-// $db   = "form_builder";
-
-// $conn = new mysqli($host, $user, $pass, $db);
-
-// if($conn->connect_error){
-//     die("Connection Failed");
-// }
-?>
 <?php
 $host = "acela.proxy.rlwy.net";
 $user = "root";
